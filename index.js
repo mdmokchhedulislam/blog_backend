7bad67c368c4ab4949b1bd757f811aba910c17e6
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-11-17 18:47:09 +0600

    blog project api creation

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+const app = express()
+dotenv.config();
+const PORT = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('BLOG api')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server is runnning port ${PORT}`);
+    
+})
